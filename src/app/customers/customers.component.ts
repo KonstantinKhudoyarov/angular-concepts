a5eaf009f6f76f54228c8aed3456c6724ba98234
KonstantinKhudoyarov <kostya040390@gmail.com>
2020-01-20 15:32:07 +0200

    change local git config

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -17,7 +17,8 @@ export class CustomersComponent implements OnInit {
       {id: 1, name: 'John Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10)},
       {id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
       {id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2016, 5, 11)},
-      {id: 4, name: 'Jim Andrews', city: 'New York', orderTotal: 59.99, customerSince: new Date(2018, 11, 25)}
+      {id: 4, name: 'Jim Andrews', city: 'New York', orderTotal: 59.99, customerSince: new Date(2018, 11, 25)},
+      {id: 5, name: 'Sara Johnson', city: 'Miami', orderTotal: 89.99, customerSince: new Date(2019, 5, 15)}
     ];
   }
 }
