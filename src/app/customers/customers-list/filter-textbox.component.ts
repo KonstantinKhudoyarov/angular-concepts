eb9261a7780ec77d263acb402c0e3fd475086e22
KonstantinKhudoyarov <kostya040390@gmail.com>
2020-01-22 10:48:51 +0200

    Add basic styles and data binding to customers-list

--- src/app/customers/customers-list/filter-textbox.component.ts
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component ({
   selector: 'app-filter-textbox',
-  template: 'Filter: <input type="text">'
+  styleUrls: ['./customers-list.component.scss'],
+  template: 'Filter: <input type="text" class="filter-box">'
 })
 
 export class FilterTextboxComponent implements OnInit {
