2ac886fbdee4a922955f9229dabcb447599e7406
KonstantinKhudoyarov <kostya040390@gmail.com>
2020-01-23 10:10:17 +0200

    Pass data from parent to child with input binding

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 import {Customer} from '../../Shared/interfaces';
 
@@ -10,20 +10,24 @@ import {Customer} from '../../Shared/interfaces';
 
 export class CustomersListComponent implements OnInit {
 
-  filteredCustomers: Customer[] = [
-    {id: 1, name: 'John Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10)},
-    {id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
-    {id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2016, 5, 11)},
-    {id: 4, name: 'Jim Andrews', city: 'New York', orderTotal: 59.99, customerSince: new Date(2018, 11, 25)},
-    {id: 5, name: 'Sara Johnson', city: 'Miami', orderTotal: 89.99, customerSince: new Date(2019, 5, 15)}
-  ];
+  private _customers: Customer[] = [];
+
+  @Input()
+  set customers(value: Customer[]) {
+    this.filteredCustomers = this._customers = value;
+    this.calculateOrders();
+  }
+
+  get customers(): Customer[] {
+    return this._customers;
+  }
+
+  filteredCustomers: Customer[] = [];
   customersOrderTotal: number;
 
   constructor() {}
 
-  ngOnInit() {
-    this.calculateOrders();
-  }
+  ngOnInit() {}
 
   calculateOrders() {
     this.customersOrderTotal = this.filteredCustomers.reduce((acc, next) => {
