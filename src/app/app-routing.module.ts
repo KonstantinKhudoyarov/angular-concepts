2f6b378a9dccf0ab8840573f7335970cfc864466
KonstantinKhudoyarov <kostya040390@gmail.com>
2020-01-31 14:18:28 +0200

    Add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/customers'},
+  {path: '**', pathMatch: 'full', redirectTo: '/customers'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
